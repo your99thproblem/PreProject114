01b315193d0326ea624ed3253562fd1b29b38271
dshkalikov <bern1991@gmail.com>
2022-05-20 18:01:03 +0300

    add connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private String dbUser = "admin";
+    private String dbPass = "Denis_16";
+    private String url = "jdbc:mysql://10.115.115.61:3306/KATA";
+
+
+    Connection dbConnection;
+
+    public Connection getDbConnection()
+            throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            dbConnection = DriverManager.getConnection(url, dbUser, dbPass);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return dbConnection;
+    }
 }
+
